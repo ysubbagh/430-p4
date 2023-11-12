17e0fda637ecc42f31c72c78ac56cbdd5765cb6d
ysubbagh <ydns10@gmail.com>
2023-11-12 00:51:30 +0000

    setup file input

--- memory.c
+++ memory.c
@@ -21,10 +21,59 @@ int fetchline(char **line) {
   return n;
 }
 
+//helper function for R command, accept and process input from a file
+bool acceptFile(char* inFile){
+    FILE *in = fopen(inFile, "r");
+    if(in == NULL){
+        perror("Error: file could not be opened.");
+        return false;
+    }
+    //setup for parsing
+    char *line;
+
+    while(line = fgetc(in)){
+        if(line == EOF){ //reached the end of the file
+
+        }
+    }
+
+
+    //finish reading from file
+    fclose(in);
+    return true;
+}
+
 //process the line entered
 bool processLine(char* line){
-    if(equal(line, "E")){return false;} //exit command
-    
+    //tokenize line
+    char *token;
+    char *args[5]; //max size is 4 for A command, extra space for NULL
+    int argsize = 0;
+    //get args from user command
+    token = strtok(line, " ");
+    while(token != NULL){
+        if(argsize >= 4){return false;} //invalid access / length 
+        args[argsize++] = token;
+    }
+    args[argsize] = NULL;
+
+    //check command value
+    if(equal(args[0], "E")){//exit command
+        return false;
+    }else if(equal(args[0], "S")){ //memory pool state
+        //stuff
+    }else if(equal(args[0], "C")){ //compact the memory pool
+        //stuff
+    }else if(equal(args[0], "F")){ //free allocations to certain process
+        //stuff
+    }else if(equal(args[0], "A")){ //allocate to certain process
+        //stuff
+    }else if(equal(args[0], "R")){ //read in from file
+        return acceptFile(args[1]);
+    }else{ //error / invalid entry
+        perror("Error: Invalid command.");
+        return false;
+    }
 }
 
 int interactiveShell(){
